969ad0442edfe21001bb06a772df4c096a8d411a
Rohith Kattamuri <50071276+rkat7@users.noreply.github.com>
2019-07-03 15:23:54 +0530

    Create CFModel.py

---
+++ CFModel.py
@@ -0,0 +1,3 @@
+# Collaborative based system predicts what a particular user might like based on what other similar users like.
+# Leveraging the item similarity based collaborative filtering model
+
